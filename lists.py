00dd652b1f3c1bdd96e814a77b8b5f6ba556e53e
7gibson <gibsonsevens@gmail.com>
2023-03-28 14:23:15 +0300

    python lists

--- lists.py
+++ lists.py
@@ -3,3 +3,20 @@ print(fruits[2])  #to print
 fruits.append('grapes') #to add another fruit in the list
 print(fruits) #to print the new list
 fruits.remove('apples') #to remove apple in the list
+fruits.append('bananas')
+print(fruits)
+print(fruits[-1])#printing by negative indexing
+fruits.extend(['lemon','watermellon'])#adding more than two
+print(fruits)
+
+my_list = [1, 2,4, 5]
+my_list.extend([5,6,7])
+games=['football','basketball','tennis','ball']
+
+games.insert(2,'handball')
+my_list.insert(2,3)
+print(my_list)
+print(games)
+
+print(games)
+print(my_list)
