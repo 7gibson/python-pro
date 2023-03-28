bbab8cb5f51be77e31460cc884250ca8eed14e7b
7gibson <gibsonsevens@gmail.com>
2023-03-28 14:03:56 +0300

    python lists

--- lists.py
+++ lists.py
@@ -1,5 +1,5 @@
 fruits=['apples','mangoes','orange','avocando']
-print(fruits[2])
-fruits.append('grapes')
-print(fruits)
-fruits.remove('apples')
+print(fruits[2])  #to print 
+fruits.append('grapes') #to add another fruit in the list
+print(fruits) #to print the new list
+fruits.remove('apples') #to remove apple in the list
